14c868cefe398e2df9d679962ed64813c7b9ad14
Kelly Rankin <krankin@voltsystems.net>
2021-01-21 15:56:15 -0800

    Adding backend code

---
+++ backend/WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public MyClass Get()
+        {
+            return new MyClass { Property1 = "something" };
+        }
+    }
+}
+
+public class MyClass
+{
+    public string Property1 { get; set; }
+}
